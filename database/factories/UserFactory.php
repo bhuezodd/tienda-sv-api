259825ea9f4273259787b7c9b8d604c863732382
JSebasART <40402037+JSebasART@users.noreply.github.com>
2021-01-16 00:40:46 -0600

    ✨ Add User M and S

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -4,6 +4,7 @@ namespace Database\Factories;
 
 use App\Models\User;
 use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Str;
 
 class UserFactory extends Factory
@@ -25,8 +26,9 @@ class UserFactory extends Factory
         return [
             'name' => $this->faker->name,
             'email' => $this->faker->unique()->safeEmail,
+            'phone'=>$this->faker->randomNumber(8, true),
             'email_verified_at' => now(),
-            'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+            'password' => Hash::make(Str::random(10)), // password
             'remember_token' => Str::random(10),
         ];
     }
---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Str;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        User::create([
+            'name'=>'john doe',
+            'email'=>'johndoe@gmail.com',
+            'phone'=>'10000000',
+            'email_verified_at' => now(),
+            'password' => Hash::make('qwerty123'), // password
+            'remember_token' => Str::random(10),
+            
+            ]);
+        User::factory(10)->create();
+    }
+}
